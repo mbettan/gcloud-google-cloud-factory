ff375816d447be2b06f99d54e57a9d9e8ec0612e
Michaël Bettan <bettan.michael@gmail.com>
2020-05-06 16:02:16 -0400

    Delete create-project.sh

--- create-project.sh#!/bin/create-project.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export MYORG_ID=`gcloud organizations list --format="value(ID)"`
-export BILLING_ID=`gcloud alpha billing accounts list --format="value(ACCOUNT_ID)"`
-export PRJ_ID=`printf $MYORG_ID | tail -c 6` 
-export PRJ_NAME="my-gcp-project"
-
-gcloud projects create $PRJ_NAME-$PRJ_ID
-gcloud alpha billing projects link $PRJ_NAME-$PRJ_ID --billing-account $BILLING_ID
-gcloud services enable compute.googleapis.com --project $PRJ_NAME-$PRJ_ID
